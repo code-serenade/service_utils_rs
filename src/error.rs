a7d827dfc5d1d34b66333ff8d6f23099c78d5623
Ancient237 <arthas.you@163.com>
2024-08-08 15:08:29 +0800

    fix: error.rs jwt feature

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,14 @@
-use jsonwebtoken;
 use thiserror::Error;
+
+#[cfg(feature = "jwt")]
+use jsonwebtoken;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("auth error: {0}")]
     AuthError(String),
 
+    #[cfg(feature = "jwt")]
     #[error("JWT error: {0}")]
     JwtError(#[from] jsonwebtoken::errors::Error),
 
