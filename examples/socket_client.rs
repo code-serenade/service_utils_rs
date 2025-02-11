eb800f390aaa1c0993755c4411e799831f269229
Ancient <arthas.you@163.com>
2025-02-11 17:35:03 +0800

    fix: reconnet websocket

--- examples/socket_client.rs
+++ examples/socket_client.rs
@@ -1,6 +1,5 @@
 use service_utils_rs::services::websocket::client::WebSocketClient;
 use std::error::Error;
-use tokio::runtime::Runtime;
 use tokio::time::Duration;
 
 #[tokio::main]
@@ -12,14 +11,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // 发送一个消息到 WebSocket 服务器
     let msg = "Hello WebSocket".to_string();
     client.send_message(msg).await?;
-
-    // 定时发送 Ping 消息（例如每 30 秒）
-    // let ping_interval = Duration::from_secs(30);
-    // tokio::spawn(async move {
-    //     if let Err(e) = client.send_ping(ping_interval).await {
-    //         eprintln!("Error sending ping: {}", e);
-    //     }
-    // });
 
     // 保持主线程运行一段时间，模拟长时间的 WebSocket 客户端运行
     // 这里设置为 60 秒，你可以根据需求调整
--- src/services/websocket/client.rs
+++ src/services/websocket/client.rs
@@ -23,8 +23,6 @@ type SocketWriter = SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Messag
 type MsgReciver = Receiver<String>;
 /// WebSocket 客户端结构体
 pub struct WebSocketClient {
-    url: String,
-    connection: ClientConnection,
     rt: Sender<String>,
 }
 
@@ -33,55 +31,31 @@ impl WebSocketClient {
     pub async fn new(url: String) -> Result<Self> {
         let (rt, rx) = mpsc::channel::<String>(4);
         let connection = connect(&url, rt.clone()).await?;
-        // let rx_clone = rx.clone();
-        let mut client = Self {
-            url,
-            connection,
-            rt,
-        };
+        let rt_clone = rt.clone();
+        let client = Self { rt: rt_clone };
         // let mut client_clone = client.clone();
-        // tokio::spawn(async move {
-        handle_reconnect(&mut client, rx).await;
-        // });
+        tokio::spawn(async move {
+            let _ = handle_reconnect(url, connection, rx, rt.clone()).await;
+        });
         Ok(client)
     }
 
     /// 发送消息到 WebSocket 服务器
     pub async fn send_message(&self, msg: String) -> Result<()> {
-        self.connection
-            .msg_sender
+        self.rt
             .send(msg)
             .await
             .map_err(|e| Error::ErrorMessage(e.to_string()))?;
         Ok(())
     }
-
-    /// 尝试重连 WebSocket
-    async fn reconnect(&mut self) -> Result<()> {
-        let mut retries = 5; // 最大重连次数
-        while retries > 0 {
-            match connect(&self.url, self.rt.clone()).await {
-                Ok(connection) => {
-                    self.connection = connection; // 替换当前的 WebSocket 客户端
-                    println!("重连成功");
-                    return Ok(()); // 成功重连后返回
-                }
-                Err(e) => {
-                    retries -= 1;
-                    println!("重连失败，剩余重试次数: {}, 错误: {}", retries, e);
-                    if retries > 0 {
-                        time::sleep(Duration::from_secs(5)).await; // 失败时等待 5 秒后重试
-                    }
-                }
-            }
-        }
-        Err(Error::ErrorMessage(
-            "重连失败，已达最大重试次数".to_string(),
-        ))
-    }
 }
 
-async fn receive_message(mut socket_reader: SocketReader, rt: Sender<String>) -> Result<()> {
+async fn receive_message(
+    mut socket_reader: SocketReader,
+    rt: Sender<String>,
+    exit_tx_send_msg: Sender<()>,
+    exit_tx_ping: Sender<()>,
+) -> Result<()> {
     loop {
         match socket_reader.next().await {
             Some(Ok(Message::Text(text))) => {
@@ -105,6 +79,10 @@ async fn receive_message(mut socket_reader: SocketReader, rt: Sender<String>) ->
             }
             Some(Err(e)) => {
                 println!("接收消息时出错: {}", e);
+
+                // 发送退出信号，通知其他进程退出
+                let _ = exit_tx_send_msg.send(()); // 发送退出信号给 handle_send_msg
+                let _ = exit_tx_ping.send(()); // 发送退出信号给 send_ping
                 let _ = rt
                     .send("reconnect".to_string())
                     .await
@@ -125,44 +103,120 @@ pub async fn connect(url: &str, rt: Sender<String>) -> Result<ClientConnection> 
     let (socket_writer, socket_reader) = socket.split();
 
     let (msg_sender, msg_reciever) = mpsc::channel::<String>(4);
-    tokio::spawn(receive_message(socket_reader, rt));
-    tokio::spawn(handle_send_msg(msg_reciever, socket_writer));
-    tokio::spawn(send_ping(msg_sender.clone(), Duration::from_secs(10)));
+    let (exit_tx_send_msg, exit_rx_send_msg) = mpsc::channel::<()>(1); // 发送消息任务退出通道
+    let (exit_tx_ping, exit_rx_ping) = mpsc::channel::<()>(1); // Ping任务退出通道
+
+    // 启动接收消息任务
+    tokio::spawn(receive_message(
+        socket_reader,
+        rt.clone(),
+        exit_tx_send_msg.clone(),
+        exit_tx_ping.clone(),
+    ));
+
+    // 启动发送消息任务，传递退出通道
+    tokio::spawn(handle_send_msg(
+        msg_reciever,
+        socket_writer,
+        exit_rx_send_msg,
+    ));
+
+    // 启动 Ping 任务，传递退出通道
+    tokio::spawn(send_ping(
+        msg_sender.clone(),
+        Duration::from_secs(10),
+        exit_rx_ping,
+    ));
 
     let connection = ClientConnection { msg_sender };
     println!("成功连接到 WebSocket 服务器");
     Ok(connection)
 }
 
-async fn handle_send_msg(mut rx: MsgReciver, mut writer: SocketWriter) -> Result<()> {
-    while let Some(msg) = rx.recv().await {
-        if let Err(e) = writer.send(Message::text(msg)).await {
-            eprintln!("Error sending message: {}", e);
-            return Err(Error::WsError(e));
+async fn handle_send_msg(
+    mut rx: MsgReciver,
+    mut writer: SocketWriter,
+    mut exit_rx: Receiver<()>,
+) -> Result<()> {
+    loop {
+        tokio::select! {
+            Some(msg) = rx.recv() => {
+                if let Err(e) = writer.send(Message::text(msg)).await {
+                    eprintln!("Error sending message: {}", e);
+                    return Err(Error::WsError(e));
+                }
+            }
+            _ = exit_rx.recv() => {
+                println!("Received exit signal, exiting send message task.");
+                break;  // 退出任务
+            }
         }
     }
-    println!("recieve_msg task is exiting due to connection drop or other error.");
     Ok(())
 }
 
-/// 定时发送 Ping 消息
-async fn send_ping(msg_sender: super::MsgSender, interval: Duration) -> Result<()> {
+async fn send_ping(
+    msg_sender: super::MsgSender,
+    interval: Duration,
+    mut exit_rx: Receiver<()>,
+) -> Result<()> {
     let mut interval_timer = time::interval(interval);
 
     loop {
-        interval_timer.tick().await;
-
-        msg_sender
-            .send("ping".to_string())
-            .await
-            .map_err(|e| Error::ErrorMessage(e.to_string()))?;
-    }
-}
-
-async fn handle_reconnect(c: &mut WebSocketClient, mut rx: Receiver<String>) {
-    while let Some(msg) = rx.recv().await {
-        if msg == "reconnect" {
-            let _ = c.reconnect().await;
+        tokio::select! {
+            _ = interval_timer.tick() => {
+                msg_sender
+                    .send("ping".to_string())
+                    .await
+                    .map_err(|e| Error::ErrorMessage(e.to_string()))?;
+            }
+            _ = exit_rx.recv() => {
+                println!("Received exit signal, exiting ping task.");
+                break;  // 退出任务
+            }
         }
     }
+    Ok(())
+}
+
+async fn handle_reconnect(
+    url: String,
+    mut connection: ClientConnection,
+    mut rx: Receiver<String>,
+    rt: Sender<String>,
+) -> Result<()> {
+    while let Some(msg) = rx.recv().await {
+        if msg == "reconnect" {
+            connection = reconnect(&url, rt.clone()).await?;
+        } else {
+            connection
+                .msg_sender
+                .send(msg)
+                .await
+                .map_err(|e| Error::ErrorMessage(e.to_string()))?;
+        }
+    }
+    Ok(())
+}
+
+async fn reconnect(url: &str, rt: Sender<String>) -> Result<ClientConnection> {
+    let mut retries = 5; // 最大重连次数
+    while retries > 0 {
+        match connect(url, rt.clone()).await {
+            Ok(connection) => {
+                println!("重连成功");
+                return Ok(connection); // 成功重连后返回
+            }
+            Err(e) => {
+                retries -= 1;
+                println!("重连失败，剩余重试次数: {}, 错误: {}", retries, e);
+                if retries > 0 {
+                    time::sleep(Duration::from_secs(5)).await; // 失败时等待 5 秒后重试
+                }
+            }
+        }
+    }
+    Err(Error::ErrorMessage(
+        "重连失败，已达最大重试次数".to_string(),
+    ))
 }
