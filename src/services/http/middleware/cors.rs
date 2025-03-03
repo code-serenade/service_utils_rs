0a0049982b95285f6fab0c6a828f2cddb0090d55
Ancient <arthas.you@163.com>
2025-03-03 18:49:07 +0800

    fix: cros wrong

--- src/services/http/middleware/cors.rs
+++ src/services/http/middleware/cors.rs
@@ -5,5 +5,4 @@ pub fn create_cors() -> CorsLayer {
         .allow_methods(Any) // 允许任意 HTTP 方法
         .allow_origin(Any) // 允许任意来源
         .allow_headers(Any) // 允许任意请求头，包括 Content-Type
-        .allow_credentials(true)
 }
